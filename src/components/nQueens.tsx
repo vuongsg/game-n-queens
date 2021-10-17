18afffb05a23aca80c6186695bb836bbe1e7b499
vuongsg <vuong.goteborg@gmail.com>
2021-10-18 05:39:17 +0700

    Code refactoring

--- src/components/nQueens.tsx
+++ src/components/nQueens.tsx
@@ -1,169 +1,138 @@
-import React, { ReactElement, useEffect, useRef, useCallback } from 'react';
+import React, { ReactElement, useEffect, MouseEventHandler } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { Container, Grid, MenuItem, Select, InputLabel } from "@material-ui/core";
-import { changeBoard } from "../slices/nQueens-slices";
-import { RootType } from "../store"; 
+import { makeStyles } from "@material-ui/core/styles";
+import { BoardState, changeBoard, changeQueen, setErrorBoard, removeErrorBoard } from "../slices/nQueens-slices";
+import { RootType } from "../store";
 import { NqueenProducer } from '../producers/nQueensProducer';
 import './nQueens.scss';
 
-export const Nqueens = (): ReactElement => {
-    let board = useSelector<RootType>(state => state.nQueens) as boolean[][];   //board state
-    const dispatch = useDispatch();
-    let initialRowsBoard: ReactElement[] = [];
-    const rowsBoard = useRef(initialRowsBoard);     //display UI
-    let initialCurrentTempboard = [...board];       //save board state immediately each change
-    let currentTempBoard = useRef(initialCurrentTempboard);
+const useStyles = makeStyles(theme => ({
+    none: {
+        backgroundImage: ''
+    },
+    queen: {
+        backgroundImage: `url('${process.env.PUBLIC_URL + '/img/queen-piece.jpg'}')`,
+        backgroundSize: 'cover'
+    },
+    whiteQueen: {
+        backgroundImage: `url('${process.env.PUBLIC_URL + '/img/queen-white-piece.jpg'}')`,
+        backgroundSize: 'cover'
+    },
+    ul: {
+        listStyle: 'none'
+    },
+    border: {
+        border: '4px solid red'
+    }
+}));
 
-    useEffect(() => {
-        return () => {
-            dispatch(changeBoard(currentTempBoard.current));
+function cloneArray<T>(arr: T[][]): T[][] {
+    const size = arr.length;
+    const b: T[][] = Array(size);
+
+    for (let i = 0; i < size; i++) {
+        b[i] = Array(size);
+
+        for (let k = 0; k < size; k++) {
+            b[i][k] = arr[i][k];
         }
-    }, []);
+    }
+
+    return b;
+}
+
+export const Nqueens = (): ReactElement => {
+    let boardState = useSelector<RootType>(state => state.nQueens) as BoardState;   //board state
+    const dispatch = useDispatch();
+    const classes = useStyles();
 
     useEffect(() => {
-        const btnCheckAnswer = document.querySelector('#btn-check-answer') as HTMLButtonElement;
-        const rows = board.length;
-        const eachQueenEachRow: boolean[] = Array(rows).fill(false);
+        let eachQueenEachRow = false;
 
-        for (let i = 0; i < rows; i++) {
-            for (let k = 0; k < rows; k++) {
-                if (board[i][k]) {
-                    eachQueenEachRow[i] = true;
+        for (let i = 0, n = boardState.size; i < n; i++) {
+            let isOnThisRow = false;
+
+            for (let k = 0; k < n; k++) {
+                if (boardState.board[i][k]) {
+                    isOnThisRow = true;
+                    break;
                 }
+            }
+
+            if (isOnThisRow) {
+                if (i === n - 1) {
+                    eachQueenEachRow = true;
+                    break;
+                }
+            } else {
+                break;
             }
         }
 
-        if (eachQueenEachRow.filter(m => !m).length > 0) {
-            btnCheckAnswer.setAttribute('disabled', 'disabled');
-            (document.querySelector('#div-fake-check-answer') as HTMLDivElement).style.cursor = 'not-allowed';
-        } else {
+        const btnCheckAnswer = document.querySelector('#btn-check-answer') as HTMLButtonElement;
+        if (eachQueenEachRow) {
             btnCheckAnswer.removeAttribute('disabled');
             (document.querySelector('#div-fake-check-answer') as HTMLDivElement).style.cursor = 'auto';
+        } else {
+            btnCheckAnswer.setAttribute('disabled', 'disabled');
+            (document.querySelector('#div-fake-check-answer') as HTMLDivElement).style.cursor = 'not-allowed';
         }
-    }, [board]);
-
-    const drawBoard = useCallback(() => {
-        const setQueen = (e:React.MouseEvent<HTMLDivElement>): void => {
-            const gridBoard = document.querySelector('#grid-board');
-            let cellBoard = e.target as HTMLDivElement;
-
-            if (gridBoard !== null && cellBoard !== null && cellBoard.className.indexOf('cell-board') !== -1) {
-                //set new value for currentTempBoard, check whether each queen each row or not
-                const eachQueenEachRow: boolean[] = Array(rows).fill(false);
-                const tempBoard = Array(rows);
-
-                for (let i = 0; i < rows; i++) {
-                    tempBoard[i] = Array(rows).fill(false);
-
-                    for (let k = 0; k < rows; k++) {
-                        tempBoard[i][k] = currentTempBoard.current[i][k];
-                        if (tempBoard[i][k]) {
-                            eachQueenEachRow[i] = true;
-                        }
-                    }
-                }
-
-                const row = parseInt(cellBoard.classList[0].substr(cellBoard.classList[0].indexOf('row') + 'row'.length));
-                const col = parseInt(cellBoard.classList[1].substr(cellBoard.classList[1].indexOf('col') + 'col'.length));
-                for (let k = 0, n = currentTempBoard.current.length; k < n; k++) {
-                    tempBoard[row][k] = (k === col);
-                    if (tempBoard[row][k]) {
-                        eachQueenEachRow[row] = true;
-                    }
-                }
-
-                currentTempBoard.current = [...tempBoard];
-
-                const btnCheckAnswer = document.querySelector('#btn-check-answer') as HTMLButtonElement;
-
-                if (eachQueenEachRow.filter(m => !m).length > 0) {
-                    btnCheckAnswer.setAttribute('disabled', 'disabled');
-                    (document.querySelector('#div-fake-check-answer') as HTMLDivElement).style.cursor = 'not-allowed';
-                } else {
-                    btnCheckAnswer.removeAttribute('disabled');
-                    (document.querySelector('#div-fake-check-answer') as HTMLDivElement).style.cursor = 'auto';
-                }
-
-                const elementsSameRow = gridBoard.getElementsByClassName(cellBoard.classList[0]);
-
-                //reset background for all elements in that row
-                if (elementsSameRow != null) {
-                    for (let i = 0, n = elementsSameRow.length; i < n; i++) {
-                        const cell = elementsSameRow[i] as HTMLDivElement;
-                        cell.style.backgroundImage = 'none';
-                        cell.style.border = 'none';
-                    }
-                }
-    
-                if (window.getComputedStyle(cellBoard, null).getPropertyValue('background-color') === 'rgb(255, 255, 255)') {
-                    cellBoard.style.backgroundImage = `url('${process.env.PUBLIC_URL + '/img/queen-piece.jpg'}')`;
-                } else {
-                    cellBoard.style.backgroundImage = `url('${process.env.PUBLIC_URL + '/img/queen-white-piece.jpg'}')`;
-                }
-    
-                cellBoard.style.backgroundSize = 'cover';
-            } 
-        };
-        
-        currentTempBoard.current = [...board];
-        const gridBoard = document.querySelector('#grid-board');
-        if (gridBoard !== null) {   //at initialize, gridBoard does not exist
-            const rowsCollection = Array.from(document.getElementsByClassName('row-board'));
-
-            for (let row of rowsCollection) {
-                const cellsCollection = Array.from(row.getElementsByClassName('cell-board'));
-
-                for (let cell of cellsCollection) {
-                    const cellDiv = cell as HTMLDivElement;
-                    cellDiv.style.backgroundImage = 'none';
-                    cellDiv.style.removeProperty('border');
-                }
-            }
-        }
-
-        const rows = board.length;
-        document.title = `Game ${rows} x ${rows} queens`;
-        rowsBoard.current = [];
-
-        for (let i = 0; i < rows; i++) {
-            const cells: ReactElement[] = [];
-
-            for (let k = 0; k < rows; k++) {
-                let isWhiteCell = ((k & 1) === 0 && (i & 1) === 0) || ((k & 1) === 1 && (i & 1) === 1);
-                const classNames = `row${i} col${k} cell-board ${isWhiteCell ? 'white' : 'black'}`;
-
-                if (board[i][k]) {
-                    cells.push(React.createElement('div', { className: classNames, 
-                        style: { backgroundImage: classNames.indexOf('white') !== -1 ? `url('${process.env.PUBLIC_URL + '/img/queen-piece.jpg'}')`
-                                                                                      : `url('${process.env.PUBLIC_URL + '/img/queen-white-piece.jpg'}')`,
-                                backgroundSize: 'cover'}, 
-                        tabIndex: 0, onMouseDown: setQueen }));
-                } else {
-                    cells.push(React.createElement('div', { className: classNames, tabIndex: 0, onMouseDown: setQueen }));
-                }
-            }
-
-            const rowClassNames = `row-board ${i === 0 ? '' : 'second-more'}`;
-            rowsBoard.current.push(React.createElement('div', { className: rowClassNames }, cells));
-        }
-    }, [board]);
+    }, []);
 
     /**
      * cbx Size change event
      * @param e 
      */
     const changeSize = (e:React.ChangeEvent<{value: unknown}>): void => {
-        reInitializeBoard(parseInt(e.target.value as string));
+        const size = parseInt(e.target.value as string);
+        dispatch(changeBoard(size));
     }
 
-    const reInitializeBoard = (size: number): void => {
-        let newBoard: boolean[][] = Array(size);
-        for (let i = 0; i < size; i++) {
-            newBoard[i] = Array(size).fill(false);
+    const setQueen = (e: MouseEventHandler<HTMLDivElement>, rowIdx: number, colIdx: number): void => {
+        const size = boardState.size;
+        const tempBoard = cloneArray(boardState.board);
+
+        for (let k = 0, n = size; k < n; k++) {
+            if (k !== colIdx) {
+                tempBoard[rowIdx][k] = false;
+            } else {
+                tempBoard[rowIdx][k] = true;
+            }
         }
 
-        dispatch(changeBoard(newBoard));
-    }
+        let eachQueenEachRow = false;
+        for (let i = 0; i < size; i++) {
+            let isOnThisRow = false;
+
+            for (let k = 0; k < size; k++) {
+                if (tempBoard[i][k]) {
+                    isOnThisRow = true;
+                    break;
+                }
+            }
+
+            if (isOnThisRow) {
+                if (i === size - 1) {
+                    eachQueenEachRow = true;
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        const btnCheckAnswer = document.querySelector('#btn-check-answer') as HTMLButtonElement;
+        if (eachQueenEachRow) {
+            btnCheckAnswer.removeAttribute('disabled');
+            (document.querySelector('#div-fake-check-answer') as HTMLDivElement).style.cursor = 'auto';
+        } else {
+            btnCheckAnswer.setAttribute('disabled', 'disabled');
+            (document.querySelector('#div-fake-check-answer') as HTMLDivElement).style.cursor = 'not-allowed';
+        }
+
+        dispatch(changeQueen(tempBoard));
+    };
 
     /**
      * btn 'Check Answer' click event
@@ -176,62 +145,77 @@ export const Nqueens = (): ReactElement => {
             return;
         }
 
-        //construct new board from current UI
-        const gridBoard = document.querySelector('#grid-board');
+        const answer = NqueenProducer.checkBoardValid(boardState.board);
 
-        if (gridBoard !== null) {
-            const answer = NqueenProducer.checkBoardValid(currentTempBoard.current);
+        if (answer.size === 0) {
+            dispatch(removeErrorBoard())
+        } else {
+            (document.querySelector('#divCongrats') as HTMLDivElement).style.display = 'none';
+            const tempErrBoard = cloneArray(boardState.errorBoard);
 
-            if (answer.size === 0) {
-                alert('Congratulations! You have an amazing play *__*');
-                removeBorderCells(gridBoard);
-            } else {
-                const rowsCollection = Array.from(gridBoard.getElementsByClassName('row-board'));
-
-                for (let row of rowsCollection) {
-                    const cellsCollection = Array.from(row.getElementsByClassName('cell-board'));
-
-                    for (let cell of cellsCollection) {
-                        const cellUI = cell as HTMLDivElement;
-                        const classList = [cellUI.classList[0], cellUI.classList[1]].join(' ');
-
-                        if (answer.has(classList)) {
-                            cellUI.style.border = '4px solid red';
-                            answer.delete(classList);
-                            break;
-                        } else {
-                            cellUI.style.border = 'none';
-                        }
+            for (let i = 0, n = boardState.size; i < n; i++)
+                for (let k = 0; k < n; k++) {
+                    const classList = `row${i} col${k}`;
+                    if (answer.has(classList)) {
+                        tempErrBoard[i][k] = true;
+                        answer.delete(classList);
+                        break;
+                    } else {
+                        tempErrBoard[i][k] = false;
                     }
                 }
-            }
+
+            dispatch(setErrorBoard(tempErrBoard));
         }
     }
 
-    const removeBorderCells = (gridBoard: Element) => {
-        if (gridBoard !== null) {
-            const rowsCollection = Array.from(gridBoard.getElementsByClassName('row-board'));
+    const getRowClassNames = (rowIdx: number): string => {
+        let className = 'row-board';
 
-            for (let row of rowsCollection) {
-                const cellsCollection = Array.from(row.getElementsByClassName('cell-board'));
-
-                for (let cell of cellsCollection) {
-                    const cellUI = cell as HTMLDivElement;
-                    cellUI.style.border = 'none';
-                }
-            }
+        if (rowIdx > 0) {
+            className += ' second-more';
         }
+
+        return className;
     }
 
-    drawBoard();    //draw board at initialize
+    const getCellClassNames = (rowIdx: number, colIdx: number): string => {
+        let className = 'cell-board';
+
+        const isWhiteCell = ((colIdx & 1) === 0 && (rowIdx & 1) === 0) || ((colIdx & 1) === 1 && (rowIdx & 1) === 1);
+        if (isWhiteCell) {
+            className += ' white';
+        } else {
+            className += ' black';
+        }
+
+        if (boardState.board[rowIdx][colIdx]) {
+            if (isWhiteCell) {
+                className += ` ${classes.queen}`;
+            } else {
+                className += ` ${classes.whiteQueen}`;
+            }
+        }
+
+        if (boardState.errorBoard[rowIdx][colIdx]) {
+            className += ` ${classes.border}`;
+        }
+
+        return className;
+    }
 
     return (
         <Container className='main-container'>
+            <Grid id='divCongrats' container direction='row' style={{display: boardState.showCongrats ? 'block' : 'none', 
+                                                                    textAlign: 'center', padding: '10px'}}>
+                <h1 style={{color: 'blue'}}>Congratulations! You have an amazing play *__*</h1>
+            </Grid>
+
             <Grid container direction='row'>
                 <Grid item xs={12} lg={5} id='select-area' style={{textAlign: 'center'}}>
                     <Grid item xs={12}>
                         <InputLabel id='label-size'>Select size for the board</InputLabel>
-                        <Select labelId='label-size' value={board.length} onChange={changeSize}>
+                        <Select labelId='label-size' value={boardState.size} onChange={changeSize}>
                             <MenuItem value="4">4 x 4</MenuItem>
                             <MenuItem value="6">6 x 6</MenuItem>
                             <MenuItem value="8">8 x 8</MenuItem>
@@ -256,7 +240,13 @@ export const Nqueens = (): ReactElement => {
                 </Grid>
 
                 <Grid item xs={12} lg={7} justifyContent='center' id='grid-board'>
-                    {rowsBoard.current}
+                    {boardState.board.map((row, rowIdx) => 
+                        <div className={getRowClassNames(rowIdx)}>{
+                            row.map((col, colIdx) => 
+                                <div className={getCellClassNames(rowIdx, colIdx)} tabIndex={0} onMouseDown={(e: any) => setQueen(e, rowIdx, colIdx)}></div>
+                            )}
+                        </div>
+                    )}
                 </Grid>
             </Grid>
         </Container>
--- src/slices/nQueens-slices.ts
+++ src/slices/nQueens-slices.ts
@@ -1,19 +1,63 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState: boolean[][] = Array(4);
-for (let i = 0; i < 4; i++) {
-    initialState[i] = Array(4).fill(false);
+export interface BoardState {
+    size: number,
+    board: boolean[][]
+    errorBoard: boolean[][],
+    showCongrats: boolean
+}
+
+const createBoard = (size: number): boolean[][] => {
+    const arr: boolean[][] = Array(size);
+    for (let i = 0; i < size; i++) {
+        arr[i] = Array(size).fill(false);
+    }
+
+    return arr;
+}
+
+const initialState: BoardState = {
+    size: 4,
+    board: createBoard(4),
+    errorBoard: createBoard(4),
+    showCongrats: false
 }
 
 const slice = createSlice({
     name: 'nQueens',
     initialState: initialState,
     reducers: {
-        changeBoard: (state: boolean[][], action: PayloadAction<boolean[][]>) => {
-            return action.payload;
+        changeBoard: (state: BoardState, action: PayloadAction<number>) => {
+            return {
+                ...state,
+                size: action.payload,
+                board: createBoard(action.payload),
+                errorBoard: createBoard(action.payload),
+                showCongrats: false
+            }
+        },
+        changeQueen: (state: BoardState, action: PayloadAction<boolean[][]>) => {
+            return {
+                ...state,
+                board: action.payload
+            }
+        },
+        setErrorBoard: (state: BoardState, action: PayloadAction<boolean[][]>) => {
+            return {
+                ...state,
+                errorBoard: action.payload,
+                showCongrats: false
+            }
+        },
+        removeErrorBoard: (state: BoardState) => {
+            return {
+                ...state,
+                errorBoard: createBoard(state.size),
+                showCongrats: true
+            }
         }
     }
 })
 
-export const { changeBoard } = slice.actions;
+export const { changeBoard, changeQueen, setErrorBoard, removeErrorBoard } = slice.actions;
 export default slice.reducer;
